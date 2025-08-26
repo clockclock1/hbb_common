c93ac9e2d01ccd88c788c9c2632494bff3c42977
clockclock1 <santi163@163.com>
2025-08-26 12:42:35 +0800

    改为自己的ID/中继服务器

--- src/config.rs
+++ src/config.rs
@@ -100,13 +100,13 @@ const CHARS: &[char] = &[
     'm', 'n', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z',
 ];
 
-pub const RENDEZVOUS_SERVERS: &[&str] = &["47.92.28.8:1117"];
+pub const RENDEZVOUS_SERVERS: &[&str] = &["47.92.28.8"];
 pub const RS_PUB_KEY: &str = "vAb59uZ14TCLgJMQObl6fHZQsQ5NCR9+cAWx20OKHk8=";
 
-pub const RENDEZVOUS_PORT: i32 = 21116;
-pub const RELAY_PORT: i32 = 21117;
-pub const WS_RENDEZVOUS_PORT: i32 = 21118;
-pub const WS_RELAY_PORT: i32 = 21119;
+pub const RENDEZVOUS_PORT: i32 = 1116;
+pub const RELAY_PORT: i32 = 1117;
+pub const WS_RENDEZVOUS_PORT: i32 = 1118;
+pub const WS_RELAY_PORT: i32 = 1119;
 
 macro_rules! serde_field_string {
     ($default_func:ident, $de_func:ident, $default_expr:expr) => {
